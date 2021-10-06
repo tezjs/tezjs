10a0212a5530d6de04f4cb6535a5f6e66100b5ce
ajayojha <ojhaajay@outlook.com>
2021-10-06 19:58:09 +0530

    reafactor(router): remove unnecessary code

--- packages/router/src/module.ts
+++ packages/router/src/module.ts
@@ -3,9 +3,7 @@ const isFirstTime: boolean = false;
 const routerModule: any = async function router() {
     const nuxtInstance = this;
     nuxtInstance.nuxt.hook('build:before', async (builder) => {
-        if (nuxtInstance.options.tez && nuxtInstance.options.tez.strapi && isFirstTime) {
-            
-        }
+        
     })
 };
 
