462bc9c0f8dbbb703b0d8b8033a492b585b3539c
ajayojha <ojhaajay@outlook.com>
2022-05-14 08:13:40 +0530

    fix: resolve tez-slot component

--- packages/vue/src/components/tez-index.ts
+++ packages/vue/src/components/tez-index.ts
@@ -1,6 +1,7 @@
 import { defineComponent ,h, VNode} from "vue"
+import TezSlot from "./tez-slot"
 export default defineComponent({
     render() {
-            return h("TezSlot")
+            return h(TezSlot)
     }
   })
