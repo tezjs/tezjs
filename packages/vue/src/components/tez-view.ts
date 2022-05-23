4e8622a4171a58cb2c1aabc3b92080946c7b2c55
ajayojha <ojhaajay@outlook.com>
2022-05-23 20:49:48 +0530

    feat(vue): without layouts setup show auto-route page

--- packages/vue/src/components/tez-view.ts
+++ packages/vue/src/components/tez-view.ts
@@ -2,6 +2,7 @@ import { defineAsyncComponent, defineComponent ,h, KeepAlive } from "vue"
 import { mapGetters } from "vuex"
 import { componentState } from "../const/component-state";
 import TezIndex from './tez-index'
+import TezPage from "./tez-page"
 export default defineComponent({
     data(){
         return {
@@ -19,6 +20,8 @@ export default defineComponent({
     render() {
         let layoutName  = this.activeMasterPage ? `/layouts/${this.activeMasterPage.layoutName}.vue` : undefined;
         let layoutComponent = this.activeMasterPage ? componentState.tezAppOptions.layouts[layoutName] : undefined;
+        if(layoutName && !layoutComponent)
+            layoutComponent = ()=>new Promise((resolve:any)=>resolve({render(){return h(TezPage)}}))
         if(layoutName && !this.state[layoutName] && layoutComponent)
             this.state[layoutName] = this.vNode = h(defineAsyncComponent(() => layoutComponent()));
         else if(this.state[layoutName])
