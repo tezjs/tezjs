3a0e6c1e2f1ae20f98ef9a000996fcf8dffd387d
ajayojha <ojhaajay@outlook.com>
2022-05-26 18:21:17 +0530

    fix: ignore forward slash placed at the end of URL

--- packages/vue/src/funcs/get-url.ts
+++ packages/vue/src/funcs/get-url.ts
@@ -1,3 +1,5 @@
 export function getUrl(url:any) {
+    if(url && url.length > 1 && url.substr(url.length-1,url.length) === '/')
+        url = url.substr(0,url.length-1);
     return url === "/" ? "/index" : url === "/index" ? "/" : url;
 }
