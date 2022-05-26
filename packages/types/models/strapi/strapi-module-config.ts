ad2b73f5b8880a1cf4d3712e144d7cea047113e9
ajayojha <ojhaajay@outlook.com>
2022-05-26 13:06:04 +0530

    chore: remove unused imports

--- packages/types/models/strapi/strapi-module-config.ts
+++ packages/types/models/strapi/strapi-module-config.ts
@@ -3,7 +3,6 @@ import { MediaConfig } from "./media-config";
 import { OptimizationConfig } from "./optimization-config";
 import { PageCollectionConfig } from "./page-collection-config";
 import { PaginationConfig } from "./pagination-config";
-import { PayloadConfig } from "./payload-config";
 import { RobotsConfig } from "./robots.config";
 import { SerializeJsonConfig } from "./serialize-json-config";
 import { UserCredential } from "./user-credential-config";
