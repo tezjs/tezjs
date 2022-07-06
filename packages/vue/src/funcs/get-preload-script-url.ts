4f2e9ac1a96b28a21244eb995bd58bd3fc08621a
ajayojha <ojhaajay@outlook.com>
2022-07-06 19:58:23 +0530

    fix(vue): check local environment through 'PROD' variable

--- packages/vue/src/funcs/get-preload-script-url.ts
+++ packages/vue/src/funcs/get-preload-script-url.ts
@@ -1,14 +1,6 @@
-export function isLocalNetwork(hostname = window.location.hostname) {
-    return (
-      (['localhost', '127.0.0.1', '', '::1'].includes(hostname))
-      || (hostname.startsWith('192.168.'))
-      || (hostname.startsWith('10.0.'))
-      || (hostname.endsWith('.local'))
-    )
-  }
 export function getVersion(){
   return window.localStorage.getItem("tzV");
 }
 export function getPreloadScriptUrl(url:string){
-    return isLocalNetwork() ? `/tez/deps${url}/preload?v=${getVersion()}` : `/assets${url}/preload.js`
+    return !import.meta.env.PROD ? `/tez/deps${url}/preload?v=${getVersion()}` : `/assets${url}/preload.js`
 }
