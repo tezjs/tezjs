e848220046acefb7a70432683544bf7bc5509f63
ajayojha <ojhaajay@outlook.com>
2022-07-16 15:51:52 +0530

    fix(payload): add siteurl prefix in sitemap

--- packages/payload/src/domain/base-generator.ts
+++ packages/payload/src/domain/base-generator.ts
@@ -51,7 +51,7 @@ export abstract class BaseGenerator{
             pageJson["masterPage"] = replaceSpace(page.masterPageName)
         await writeFileSync(filePath, pageJson);
 
-        this.sitemap.add({sitemap:{...page.sitemap,...{loc:page.url}}});
+        this.sitemap.add({sitemap:{...page.sitemap,...{loc:`${commonContainer.getStrapiConfig().siteUrl}${page.url}`}}});
         this.redirectRoute.add(page);
         
     }
