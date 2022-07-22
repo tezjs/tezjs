8488c4529749f347fdad31ae8355396ae32b9f72
ajayojha <ojhaajay@outlook.com>
2022-07-22 16:17:26 +0530

    chore(create-tez): add loader image reference

--- packages/create-tez/template-tez-strapi-vue/frontend/tez.config.ts
+++ packages/create-tez/template-tez-strapi-vue/frontend/tez.config.ts
@@ -2,7 +2,8 @@ import {defineTezConfig } from "@tezjs/vite"
 
 export default defineTezConfig({
     client:{
-        imports:['/@/assets/index.css']
+        imports:['/@/assets/index.css'],
+        loaderImage:'/images/loader.gif'
     },
     strapi: {
         apiUri: process.env.HOST ? `http://${process.env.HOST}:1337` : `http://localhost:1337`
