c9c3f61373a1b6eb7aa7af77bb1550cd4bc25a66
ajayojha <ojhaajay@outlook.com>
2022-07-27 17:47:48 +0530

    fix(payload): incorrect curly bracket

--- packages/payload/src/domain/robot-txt-generator.ts
+++ packages/payload/src/domain/robot-txt-generator.ts
@@ -29,7 +29,7 @@ export class RobotTxtGenerator{
     }
 
     save(text:string){
-        if(this.robotsConfig && !commonContainer.getStrapiConfig().customPayloadGenerator){
+        if(this.robotsConfig && !commonContainer.getStrapiConfig().customPayloadGenerator)
          writeFileSync(this.pathResolver.robotsTxtPath,text,true);
     }
 }
