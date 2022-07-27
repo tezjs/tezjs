e252bbbb056ceab7507115d3839887ddf5f996b6
ajayojha <ojhaajay@outlook.com>
2022-07-27 17:20:36 +0530

    chore: handle non numeric prefix in component name

--- packages/vite/src/functions/get-component-name.ts
+++ packages/vite/src/functions/get-component-name.ts
@@ -1,7 +1,10 @@
 export function getComponentName(name:string){
     if(name.indexOf('-') !== -1){
-        let dashIndex = name.indexOf('-');
-        name = name.substr(dashIndex+1,name.length)
+        let splitText:any = name.split("-");
+        if(!isNaN(splitText[0])){
+            let dashIndex = name.indexOf("-");
+            name = name.substr(dashIndex + 1, name.length);
+        }
     }
     return name;
 }
