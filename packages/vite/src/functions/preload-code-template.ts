5883588a0405b7f14cd957348605bc0cc0d7850e
ajayojha <ojhaajay@outlook.com>
2022-08-10 18:01:49 +0530

    chore: revert build version changes

--- packages/vite/src/functions/preload-code-template.ts
+++ packages/vite/src/functions/preload-code-template.ts
@@ -2,11 +2,8 @@ import { commonContainer } from '@tezjs/common';
 
 export function getPreloadCodeTemplate(preloads:Array<string>){
     let code = `return import('./pre');`
-    let files = []
-    preloads.forEach(path=>files.push(`${path}?v=${commonContainer.buildOptions.buildVersion}`))
-    files = preloads;
     if(commonContainer.buildOptions.commandName === "build"){
-        code = `    return preload(()=> import('./pre.js?v=${commonContainer.buildOptions.buildVersion}'),${JSON.stringify(preloads)});`        
+        code = `    return preload(()=> import('./pre.js?'),${JSON.stringify(preloads)});`        
     }
     return `export default function(preload){
         ${code}
@@ -14,5 +11,5 @@ export function getPreloadCodeTemplate(preloads:Array<string>){
 }
 
 export function postScriptPreloadCodeTemplate(preloads:Array<string>){
-    return `()=> preload(()=> import('./post.js?v=${commonContainer.buildOptions.buildVersion}'),${JSON.stringify(preloads)})`
+    return `()=> preload(()=> import('./post.js'),${JSON.stringify(preloads)})`
 }
