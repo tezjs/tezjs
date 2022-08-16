1a124219a947768ebbdd5c85e982c03b2f9c99c0
ajayojha <ojhaajay@outlook.com>
2022-08-16 18:38:40 +0530

    fix(vite): remove 'dev' mode

--- packages/vite/src/domain/dev.ts
+++ packages/vite/src/domain/dev.ts
@@ -3,6 +3,6 @@ import { mergeConfig } from "vite";
 import { VITE_SERVER_CONFIG } from "../const/vite-server-config.const";
 
 export function dev(){
-    commonContainer.buildOptions = {buildVersion:0, mode: "dev", rootDir: process.cwd(), port: 3000, commandName:"dev" };
+    commonContainer.buildOptions = {buildVersion:0, mode: "", rootDir: process.cwd(), port: 3000, commandName:"dev" };
     return mergeConfig(VITE_SERVER_CONFIG(),{root:commonContainer.buildOptions.rootDir});
 }
