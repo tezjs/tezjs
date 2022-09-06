e307da9a3b0633cd56f1daa20ed819dbe7ba18e7
ajayojha <ojhaajay@outlook.com>
2022-09-06 20:31:41 +0530

    refactor(create-tez): update tezjs strapi blog template #17

--- packages/create-tez/template-tez-strapi-vue/frontend/components/article-card.vue
+++ packages/create-tez/template-tez-strapi-vue/frontend/components/article-card.vue
@@ -2,61 +2,66 @@
 defineProps<{ article: any }>();
 </script>
 <template>
-      <tez-link
-    :to="article.slug"
-    class="uk-link-reset"
-  >
-    <div class="card px-4 py-10">
-        <img class="bg-cover text-center overflow-hidden" v-tez-src="article.image.url" :style="{height:'391px !important'}" /> 
-      <div
-        class="
-          mt-3
-          bg-white
-          rounded-b
-          lg:rounded-b-none lg:rounded-r
-          flex flex-col
-          justify-between
-          leading-normal
-        "
-      >
-        <div class="">
-          <span v-for="category of article.categories" :key="category.name"
-            
-            class="
-              text-md text-gray-600
-              hover:text-teal-500
-              uppercase
-              transition
-              duration-500
-              ease-in-out
-              no-underline
-            "
-          >
-            {{ category.name }}
-          </span>
-          <span
-            
-            class="
-              block
-              text-black
-              font-bold
-              text-2xl
-              mb-2
-              hover:text-teal-600
-              transition
-              duration-500
-              ease-in-out
-              no-underline
-            "
-            >{{ article.title }}</span
-          >
+  <div class="w-full shadow-lg hover:shadow-2xl transition-all delay-100 ease-in rounded-md px-4 h-full mt-14">
+    <div class="card px-4 py-8 flex flex-wrap h-full">
+        <div class="h-[300px] overflow-hidden">
           
+          <tez-link
+            class="no-underline"
+            :to="article.slug"
+            :title="article.title"
+          >
+          <img v-tez-src="article.image.url" :width="article.image.width" :height="article.image.height" :alt="article.image.alternativeText">
+          </tez-link>
+        </div>
+      <div
+        class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal"
+      >
+        <div>
+          <div
+            class="mb-4 inline-block"
+            v-for="item in article.categories"
+            :key="item.name"
+          >
+            <tez-link
+              :to="item.slug"
+              class="rounded-sm bg-[#eeeeee] px-2 py-1 text-black text-xs leading-4 inline-flex mr-4 mb-0"
+              >{{ item.name }}</tez-link
+            >
+          </div>
+
+          <tez-link
+            class="no-underline"
+            :to="article.slug"
+            :title="article.title"
+          >
+            <h5
+              class="mb-2 text-2xl font-semibold tracking-tight text-black"
+            >
+              {{ article.title }}
+            </h5>
+          </tez-link>
+          <div
+            class="text-gray-700 text-base my-4"
+            v-html="[article.content.slice(0, 220).concat('...')]"
+          ></div>
         </div>
       </div>
-      <div class="flex items-center">
-        
-        <p class="mx-4">{{ article.author.name }}</p>
+      <div class="flex items-center mt-auto">
+        <tez-link
+          :to="article.author.slug"
+          class="flex items-center text-black no-underline"
+        >
+          <img
+            class="rounded-full mr-4"
+            v-tez-src="article.author.picture.url"
+            :alt="article.author.picture.alternativeText"
+            height="50"
+            width="50"
+          />
+          {{ article.author.name }}
+        </tez-link>
       </div>
     </div>
-    </tez-link>
+  </div>
 </template>
--- packages/create-tez/template-tez-strapi-vue/frontend/components/dark-footer.vue
+++ packages/create-tez/template-tez-strapi-vue/frontend/components/dark-footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer
-    class="border border-solid border-transparent border-t border-t-[#f0f0f0] py-5 overflow-hidden"
+    class="bg-[#345ca8] border border-solid border-transparent border-t border-t-[#f0f0f0] py-5 text-white overflow-hidden"
   >
     <div class="container mx-auto">
       <div class="flex flex-wrap -mx-4 items-center justify-between">
         <div class="w-full lg:w-1/5 xl:w-3/12 px-4 text-center xl:text-left">
           <tez-link to="/" title="TezJS" class="inline-flex items-center">
             <img
-                v-tez-src="'/images/Tez-logo.png'"
+                v-tez-src="'/images/tez-logo-white.png'"
                 alt="TezJS"
                 width="230"
                 height="35"
@@ -18,7 +18,7 @@
           class="w-full lg:w-3/5 xl:w-6/12 my-4 lg:my-0 px-4 text-center mx-auto mt-4"
         >
           <p class="p-0 my-0 text-center text-base">
-          Open-source MIT Licensed. <a target="_blank" title="TezJS" href="https://github.com/tezjs/tezjs" class="text-[#005baa] underline hover:no-underline">TezJS</a> | Powered by Strapi | Developed with <a href="https://tezjs.io/" target="_blank" class="text-[#005baa] underline hover:no-underline">TEZJS.io</a>
+          Open-source MIT Licensed. <a target="_blank" title="TezJS" href="https://github.com/tezjs/tezjs" class="underline hover:no-underline">TezJS</a> | Powered by Strapi | Developed with <a href="https://tezjs.io/" target="_blank" class="underline hover:no-underline">TEZJS.io</a>
           </p>
         </div>
         <div
@@ -32,7 +32,7 @@
                   class="align-baseline mx-4"
                   width="25"
                   height="25"
-                  v-tez-src="'/images/twitter.svg'"
+                  v-tez-src="'/images/twitter-white.svg'"
                   alt="Twitter"
               /></a>
             </li>
@@ -48,7 +48,7 @@
                 class="align-baseline"
                 width="25"
                 height="25"
-                v-tez-src="'/images/github-gray.svg'"
+                v-tez-src="'/images/github-white.svg'"
                 alt="GitHub"
               />
               </a>
