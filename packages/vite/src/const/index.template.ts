a83489f3ca45f90b17ffd47bbf59dc356a18ca54
ajayojha <ojhaajay@outlook.com>
2022-09-21 14:28:04 +0530

    feat(vite): improve FCP score #98

--- packages/vite/src/const/index.template.ts
+++ packages/vite/src/const/index.template.ts
@@ -2,13 +2,17 @@ import { commonContainer } from "@tezjs/common";
 import { Seo } from "../domain/seo"
 
 export const indexTemplate = (seo:Seo)=>{
+  let imageLoader:string ='';
+  if(commonContainer.tezConfig?.client?.loaderImage){
+    imageLoader = `<img src="${commonContainer.tezConfig?.client?.loaderImage}" style="position:absolute;top:20%;left:36%; margin:0 auto;" />`;
+  }
     return `<!DOCTYPE html>
 <html lang="${seo.htmlMeta.lang || 'en'}">
 <head>
         ${seo.headChildElements}
 </head>
       <body>
-        <div id="tez_app"></div>
+        <div id="tez_app">${imageLoader}</div>
         ${commonContainer.buildOptions.commandName === "dev"? '<script type="module" src="/tez.ts"></script>' : ""}
         ${seo.bodyChildElements}
       </body>
